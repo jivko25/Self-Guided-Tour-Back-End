7dd2b89ce3b13946d2a1d26e6d476369b3790d36
StiliyanIliev27 <stiliyaniliev2705@gmail.com>
2024-06-09 13:24:30 +0300

    Added Repository for the application

---
+++ SelfGuidedTours.Infrastructure/Common/IRepository.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore.ChangeTracking;
+
+namespace SelfGuidedTours.Infrastructure.Common
+{
+    public interface IRepository
+    {
+        IQueryable<T> All<T>() where T : class;
+        IQueryable<T> AllReadOnly<T>() where T : class;
+        Task AddAsync<T>(T entity) where T : class;
+        EntityEntry Delete<T>(T entity) where T : class;
+        Task<int> SaveChangesAsync();
+        Task<T?> GetByIdAsync<T>(object id) where T : class;
+    }
+}
---
+++ SelfGuidedTours.Infrastructure/Common/Repository.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using SelfGuidedTours.Infrastructure.Data;
+
+namespace SelfGuidedTours.Infrastructure.Common
+{
+    public class Repository : IRepository
+    {
+        private readonly SelfGuidedToursDbContext context;
+
+        public Repository(SelfGuidedToursDbContext context)
+        {
+            this.context = context;
+        }
+
+        private DbSet<T> DbSet<T>() where T : class => context.Set<T>();
+
+        public async Task AddAsync<T>(T entity) where T : class
+        {
+            await context.AddAsync(entity);
+        }
+
+        public IQueryable<T> All<T>() where T : class
+        {
+            return DbSet<T>();
+        }
+
+        public IQueryable<T> AllReadOnly<T>() where T : class
+        {
+            return DbSet<T>().AsNoTracking();
+        }
+
+        public EntityEntry Delete<T>(T entity) where T : class
+        {
+            return context.Remove(entity);
+        }
+
+        public async Task<T?> GetByIdAsync<T>(object id) where T : class
+        {
+            return await DbSet<T>().FindAsync(id);
+        }
+
+        public async Task<int> SaveChangesAsync()
+        {
+            return await context.SaveChangesAsync();
+        }
+    }
+}
